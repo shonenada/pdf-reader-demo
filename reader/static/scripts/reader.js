b7b30f740fe10e775c6b774e91399f25f45308e7
shonenada <shonenada@gmail.com>
2013-08-09 21:03:31 +0800

    Added scroll action.

--- reader/static/scripts/reader.js
+++ reader/static/scripts/reader.js
@@ -1,5 +1,31 @@
 $(function(){
 
+    var get_max_page = function(){
+        return $(".page-hash").length;
+    }
+
+    var get_current_page = function(){
+        var current_page_no = 0;
+        var location = parseInt($(document).scrollTop());
+        $(".page-hash").each(function(){
+            var page_no = parseInt($(this).attr('id').replace('page-hash-', ''));
+            var page_location = parseInt($(this).offset().top);
+            if (location > (page_location-5)){
+                current_page_no++;
+            }
+        });
+        return current_page_no;
+   }
+   
+   var show_page = function(){
+        var current_page_no = get_current_page();
+        current_page_no = current_page_no == 0 ? 1 : current_page_no;
+        var total_page = get_max_page();
+        var precentage = parseInt(current_page_no * 100 / total_page);
+        $("#current-page-no").html(current_page_no + '/' + total_page + '<br />' + precentage + '%');
+   };show_page();
+
+
     var MINIMUM_SIZE = 800;
     var MAXIMUM_SIZE = 1000;
 
@@ -16,4 +42,23 @@ $(function(){
             $(".piece").animate({width: '-=50px'}, 'fast')
         }
     });
+
+   $("#arrow-up").click(function(){
+        var current_page = get_current_page();
+        if (current_page > 1){
+            $.scrollTo($("#page-hash-" + (current_page - 1)), 500);
+        }
+   })
+
+   $("#arrow-down").click(function(){
+        var current_page = get_current_page();
+        if (current_page < get_max_page()){
+            $.scrollTo($("#page-hash-" + (current_page + 1)), 500);
+        }
+   })
+
+   $(window).scroll(function(){
+        show_page();
+   });
+
 });
